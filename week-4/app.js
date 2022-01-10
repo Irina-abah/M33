c1a527dcc1709a1527ad9492d6e24b7ff4c6cb7f
Irina Abah <irin.bon@gmail.com>
2022-01-10 19:04:37 +0200

    feat: basic animal created

--- week-4/app.js
+++ week-4/app.js
@@ -1,18 +1,44 @@
-class Dog {
-  constructor(name, age) {
-    this.name = name,
-    this.age = age
+class Caterpillar {
+  constructor(name, length, legs) {
+    this.name = name;
+    this.length = length;
+    this.legs = legs;
+    this.hunger = 20;
   }
 
-  hunger = 0;
-  thirst = 0;
-  energy = 10;
+  hungry() {
+    console.log(`${this.name} wants some food!`);
+  }
 
-  play() {
-    console.log(`${this.name} is having great time playing!`)
+  eating() {
+    this.hunger--;
+    this.length++
+    console.log(`${this.name} is eating some nice food`);
   }
 }
 
-let fido = new Dog("Fido", 2);
+const hungryCaterpillar = new Caterpillar("Mike", 10, 20)
 
-console.log(fido)
+console.log(hungryCaterpillar.eating());
+
+
+
+
+// class Dog {
+//   constructor(name, age) {
+//     this.name = name,
+//     this.age = age
+//   }
+
+//   hunger = 0;
+//   thirst = 0;
+//   energy = 10;
+
+//   play() {
+//     console.log(`${this.name} is having great time playing!`)
+//   }
+// }
+
+// let fido = new Dog("Fido", 2);
+
+// console.log(fido)
